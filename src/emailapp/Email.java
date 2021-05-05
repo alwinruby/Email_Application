e2c3281074c02cefe126ec2f90dd2d40b99463bc
alwinruby <alwinsolanky@gmail.com>
2021-05-05 18:30:25 +0100

    Outline for the Email Admin class

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -2,5 +2,24 @@ package emailapp;
 
 public class Email {
 
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructor to receive the first and last names
+
+    //Ask for the department
+
+    //Generate a random password
+
+    //Set the mailbox capacity
+
+    //Set the alternate email
+
+    //Change the password
+
 
 }
